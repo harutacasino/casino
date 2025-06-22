53fad4b4efb0b9caaf1207da2f57f63b86c1f0eb
harutacasino <maikura121209@gmail.com>
2025-06-23 01:00:09 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbwrhXpS-BPHfOQ_JZpzlewRYJ5UIuRBNrV4Un9B2dc0Y0bnuK0bgFlJDal74sy-tOqbUg/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbz8LgNIeFsen5DP1OiL-Qz4dQCVlCUQpWyFe-dmSmS8RgUHxGnpt5Bjmh_9aLFY4K6CSg/exec';
